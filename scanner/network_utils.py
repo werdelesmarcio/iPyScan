e17b1f801e7e9ee24765ac087efb109cf02810d4
Werdeles Soares <werdelesmarcio@hotmail.com>
2024-10-24 18:08:36 -0300

    Atualizando os módulos

--- scanner/network_utils.py
+++ scanner/network_utils.py
@@ -1,13 +1,6 @@
 import socket
 import sys
 
-def resolve_target(target):
-    try:
-        return socket.gethostbyname(target)
-    except socket.gaierror:
-        print('Hostname could not be resolved. Exiting.')
-        sys.exit(1)
-
 def connect(target, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(0.1)
@@ -16,3 +9,10 @@ def connect(target, port):
             return True
         except (socket.timeout, socket.error):
             return False
+
+def resolve_target(target):
+    try:
+        return socket.gethostbyname(target)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting.')
+        sys.exit(1)
