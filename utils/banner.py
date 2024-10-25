e3d417b6e72095c7aae75cf873d8ddd9f759dbf5
Werdeles Soares <werdelesmarcio@hotmail.com>
2024-10-25 00:24:58 -0300

    Atualizando o sistema

--- utils/banner.py
+++ utils/banner.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # PyTCPScan Version 3.0 (beta)
+from datetime import datetime
 import os
 import time
 from colorama import init, Fore, Back, Style # Usando o colorama para inserir cor ao texto do terminal
@@ -10,12 +11,12 @@ init()
 def banner():
     time.sleep(2) # Aguardar dois segundos para iniciar a aplicação
     os.system('cls' if os.name == 'nt' else 'clear') # Limpa a tela
+    data = datetime.now()
+    
     print(Fore.RED + 
           """
-                          Welcome to iPyScan Ver.: 1.0.28    
-                          By: Werdeles Soares (gh05tb0y)    
-                          E-Mail: gh05tb0y@disroot.org
----------------------------------------------------------
+ Welcome to iPyScan v. 1.0.28. Date: """ + data.strftime('%d/%m/%Y') + """   
+ Developed by: Werdeles Soares (gh05tb0y). E-Mail: gh05tb0y@disroot.org                          
           """
         + Fore.RESET)
 
