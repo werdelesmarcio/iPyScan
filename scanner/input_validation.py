01ecd5c3a571cac6942aad57e80fe9b6534173dc
Werdeles Soares <werdelesmarcio@hotmail.com>
2025-06-12 11:39:55 -0300

    Novas atualizações do sistema.

--- scanner/input_validation.py
+++ scanner/input_validation.py
@@ -1,7 +1,20 @@
 import sys
 
-def validate_input():
+def validate_input(argv):
     if len(sys.argv) != 4:
         print('\n [!] ERROR: Missing input arguments.')
         print(' [!] Usage: ./pyTCPScan.py [target] [initial_port] [final_port]\n')
-        sys.exit(1)
+        return False
+    try:
+        initial_port = int(sys.argv[2])
+        final_port = int(sys.argv[3])
+        if initial_port < 0 or final_port < 0 or initial_port > 65535 or final_port > 65535:
+            print('\n [!] ERROR: Port numbers must be between 0 and 65535.')
+            return False
+        if initial_port > final_port:
+            print('\n [!] ERROR: Initial port cannot be greater than final port.')
+            return False
+    except ValueError:  # Verifica se as portas são números inteiros
+        print('\n [!] ERROR: Port numbers must be integers.')
+        return False
+    return True  # Retorna True se todos os parâmetros forem válidos
